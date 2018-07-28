d664c62aad62d24e6ae7b2b8ee6defa9eaabb00e
Marvelous Wololo <marvelous.wololo@gmail.com>
2018-07-28 03:24:57 -0300

    Initial Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+const js = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['react', 'es2015'],
+      plugins: ['transform-class-properties']
+    }
+  }
+}
+
+const serverConfig = {
+  mode: 'development',
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+  externals: [nodeExternals()],
+  entry: {
+    'index.js': path.resolve(__dirname, 'src/index.js')
+  },
+  module: {
+    rules: [js]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]'
+  }
+}
+
+const clientConfig = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    'home.js': path.resolve(__dirname, 'src/public/home.js')
+  },
+  module: {
+    rules: [js]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/public'),
+    filename: '[name]'
+  }
+}
+
+module.exports = [serverConfig, clientConfig]
