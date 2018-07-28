f7bf2bd39c842e6ff808f755e4fc030ccf06d169
Marvelous Wololo <marvelous.wololo@gmail.com>
2018-07-28 03:34:07 -0300

    Using Express query string

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import express from 'express'
 import path from 'path'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import { StaticRouter as Router } from 'react-router-dom'
 import Hello from './public/components/Hello'
+import MultipleRoutes from './public/components/MultipleRoutes'
 
 const app = express()
 
 app.use('/static', express.static(path.resolve(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  const name = 'Marvelous Wololo'
+  const { name = 'Marvelous Wololo' } = req.query
 
   const componentStream = ReactDOMServer.renderToNodeStream(
     <Hello name={name} />
@@ -41,6 +43,36 @@ app.get('/', (req, res) => {
 
     res.end()
   })
+})
+
+app.get('/with-react-router*', (req, res) => {
+  const context = {}
+
+  const component = ReactDOMServer.renderToString(
+    <Router location={req.url} context={context}>
+      <MultipleRoutes />
+    </Router>
+  )
+
+  const html = `
+  <!doctype html>
+    <html>
+    <head>
+      <title>document</title>
+    </head>
+    <body>
+      <div id="root">${component}</div>
+      <script src="/static/multipleRoutes.js"></script>
+    </body>
+    </html>
+  `
+
+  if (context.url) {
+    res.writeHead(301, { Location: context.url })
+    res.end()
+  } else {
+    res.send(html)
+  }
 })
 
 app.listen(3000)
