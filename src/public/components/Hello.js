95ab690634df648a7e4e92efb13a0b79c9af2e09
Marvelous Wololo <marvelous.wololo@gmail.com>
2020-04-03 18:19:22 +0200

    Add query string example

--- src/public/components/Hello.js
+++ src/public/components/Hello.js
@@ -4,6 +4,13 @@ const Hello = (props) => (
   <React.Fragment>
     <h1>Hello, {props.name}!</h1>
 
+    <p>
+      You can change this parameter by requesting <code>name</code> with a query
+      string, like:
+    </p>
+
+    <pre>/?name=John Doe</pre>
+
     <a href="/with-react-router">with React Router</a>
   </React.Fragment>
 );
